5d42fb38938ffa3db0cff0bac665404964bdd261
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-01 15:43:50 +0200

    informazioni

--- jv/script.js
+++ jv/script.js
@@ -1,13 +1,17 @@
 /* informazioni */
-
-const userAge = parseInt((prompt = "quanti anni hai ?"));
-const userkm = parseInt((prompt = "quanti km devi percorrere ?"));
-const priceporkm = (userkm * 0, 21);
+// alert("messaggio per me");
+const userAge = parseInt(prompt("quanti anni hai ?"));
+const userkm = parseInt(prompt("quanti km devi percorrere ?"));
+const priceporkm = userkm * 0.21;
 const discount20 = (priceporkm * 20) / 100;
+const tarifYoung = priceporkm - discount20;
 const discount40 = (priceporkm * 40) / 100;
+const tarifOver = priceporkm - discount40;
 
 console.log(userAge);
 console.log(userkm);
 console.log(priceporkm);
-console.log(discount20);
-console.log(discount40);
+console.log(tarifYoung);
+console.log(tarifOver);
+
+/* azioni */
