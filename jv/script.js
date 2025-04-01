b4a6147a4518ccecb71babfc04c31e0a59efebd4
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-01 17:32:10 +0200

    snellitocodiceinfo

--- jv/script.js
+++ jv/script.js
@@ -2,10 +2,8 @@
 const userAge = parseInt(prompt("quanti anni hai ?"));
 const userkm = parseInt(prompt("quanti km devi percorrere ?"));
 const priceporkm = userkm * 0.21;
-const discount20 = (priceporkm * 20) / 100;
-const tarifYoung = priceporkm - discount20;
-const discount40 = (priceporkm * 40) / 100;
-const tarifOver = priceporkm - discount40;
+const tarifYoung = priceporkm - (priceporkm * 20) / 100;
+const tarifOver = priceporkm - (priceporkm * 40) / 100;
 
 console.log("userAge", userAge);
 console.log("userkm", userkm);
